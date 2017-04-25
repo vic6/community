03cdc3e0482084bc4635ab37709e410f89ebf990
vic6 <vicmatthews116@gmail.com>
2017-04-24 17:51:48 -0700

    Create request show and index pages

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,12 +3,25 @@ class RequestsController < ApplicationController
     @requests = Request.all
   end
 
+  def show
+    @request = Request.find(params[:id])
+  end
+
   def new
+  end
+
+  def create
+    @request = Request.new(reqest_params)
+    @reqeust.save
+    redirect '/requests'
   end
 
   def edit
   end
 
-  def show
+  private
+
+  def request_params
+    params.require(:request).permit(:requester_id, :acceptor_id, package_id)
   end
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,4 +2,8 @@ class Tenant < ApplicationRecord
   belongs_to :apartment
   has_many :requests, foreign_key: :requester_id
   has_many :packages, through: :requests
+
+  # def full_name
+  #   full_name = [first_name + last_name].join()
+  # end
 end
