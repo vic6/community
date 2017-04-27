012b1724b2ef2617687007f833774c3eeca19243
vic6 <vicmatthews116@gmail.com>
2017-04-27 16:18:12 -0700

    Fix new package form

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,2 +1,16 @@
 class PackagesController < ApplicationController
+  def new
+  end
+
+  def create
+    @package = Package.new(package_params)
+    @package.save!
+    redirect_to '/requests'
+  end
+
+  private
+
+  def package_params
+    params.require(:package).permit(:name, :tracking_number)
+  end
 end
