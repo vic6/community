70cda2abcf1731fda28b6b287cc9b36c8f6e8bbc
vic6 <vicmatthews116@gmail.com>
2017-05-02 12:41:30 -0700

    Fix home route, fix header styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get '/signup', to: 'tenants#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tenants
   resources :requests
   resources :packages
 
-  root 'requests#index'
+  root 'static_pages#home'
 end
