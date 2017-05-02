15f8f19fc0bad2f57ef860dbecfa7baf23f9e4dd
vic6 <vicmatthews116@gmail.com>
2017-05-02 15:11:09 -0700

    Add custom stylesheet, add create action to tenants controller

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -2,4 +2,24 @@ class TenantsController < ApplicationController
   def show
     @tenant = Tenant.find(params[:id])
   end
+
+  def new
+    @tenant = Tenant.new
+  end
+
+  def create
+    @tenant = Tenant.create(tenant_params)
+    if @tenant.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def tenant_params
+    params.require(:tenant).permit(:first_name, :last_name,
+                                   :email, :password,
+                                   :password_confirmation)
+  end
 end
