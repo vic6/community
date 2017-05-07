8bacb503b9b16ae622934092234d5cef7f7e1246
vic6 <vicmatthews116@gmail.com>
2017-05-07 13:08:37 -0700

    Add error flash to tenant login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    tenant = Tenant.find_by(email: params[:session][:email].downcase)
+    if tenant && tenant.authenticate(params[:session][:password])
+      # login(tenant)
+    else
+      flash.now[:danger] = 'Invalid email/password'
+      render 'new'
+    end
   end
 
   def delete
