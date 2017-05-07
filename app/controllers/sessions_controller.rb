fb788c866a252ddb016137ec1f9c35e5a23066a0
vic6 <vicmatthews116@gmail.com>
2017-05-07 12:55:08 -0700

    Add login path, create session actions and routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    render 'new'
+  end
+
+  def delete
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get '/signup', to: 'tenants#new'
   post '/signup', to: 'tenants#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#delete'
 end
