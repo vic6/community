2256a8a671240abbec95a99cf26e9946073d995b
vic6 <vicmatthews116@gmail.com>
2017-05-07 16:16:53 -0700

    Finish basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,13 @@ module SessionsHelper
   def current_tenant
     @current_tenant ||= Tenant.find_by(id: session[:tenant_id])
   end
+
+  def logged_in?
+    !current_tenant.nil?
+  end
+
+  def log_out
+    session.delete(:tenant_id)
+    @current_tenant = nil
+  end
 end
